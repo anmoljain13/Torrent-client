89356b21112b9fae6560e7ef079df9c4784b743f
Anmol Abhay Jain <54735807+anmoljain13@users.noreply.github.com>
2021-07-14 19:10:21 +0530

    Completed the download portion

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const torrentParser = require('./lib/torrent_parser'); //contains code to get in
 const tracker = require('./lib/tracker');
 const torrent = torrentParser.parse(process.argv[2]);
 const download = require('./lib/download')
+// console.log(torrent);
 
 const getPeers = tracker.getPeers(torrent,(peers)=>{
     download(peers, torrent);
