ecf3c4adf7b25188e811ad7f509ec317bb46b1b2
Anmol Abhay Jain <54735807+anmoljain13@users.noreply.github.com>
2021-07-14 23:00:01 +0530

    Debugging code added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const torrentParser = require('./lib/torrent_parser'); //contains code to get information out of a torrent file
 const tracker = require('./lib/tracker');
 const torrent = torrentParser.parse(process.argv[2]);
-const download = require('./lib/download')
-// console.log(torrent);
-
+const download = require('./lib/download');
+const utils = require('./lib/utils');
+console.log(torrent);
+console.log(utils.pieceLen(torrent,9));
+console.log(utils.blockLen(torrent,9,0));
 const getPeers = tracker.getPeers(torrent,(peers)=>{
+    console.log(peers);
     download(peers, torrent);
 });
